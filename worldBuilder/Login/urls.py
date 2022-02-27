98e9813ccf9a72e04b4f5762e0c98b6ffb54365f
Katie2211 <rosehopk@gmail.com>
2022-02-27 14:58:06 +0000

    Removed /admin path from the login as it was redundent code Done URL tests for login and gardenGame (though not currently linked up) 27/02/2022 14:58

--- worldBuilder/Login/urls.py
+++ worldBuilder/Login/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('admin/', admin.site.urls),
+    ##path('admin/', admin.site.urls),
     path('login/', views.login_request, name="login"),
     path('register/', views.register_request, name="register"),
     #path('main/', include(Gardengame.urls))
