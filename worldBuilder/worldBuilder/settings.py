83d211932d2e8d96b461fe2c6cd1101d56f9f086
umennear <75434681+umennear@users.noreply.github.com>
2022-03-13 16:13:16 +0000

    Update settings.py

--- worldBuilder/worldBuilder/settings.py
+++ worldBuilder/worldBuilder/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gardenGame.apps.gardengameConfig',
-    'crispy_forms',
+    'Login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
