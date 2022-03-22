55ea819706ce338e4fd71c42ef6c270275b0578b
Katie2211 <rosehopk@gmail.com>
2022-03-22 14:44:36 +0000

    Staff Validation Merged

--- worldBuilder/Login/tests/test_forms.py
+++ worldBuilder/Login/tests/test_forms.py
@@ -13,3 +13,17 @@ class TestForm(TestCase):
     def test_NewUserForm_bad_data(self):
         form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : '12345', "password2":'12345'})
         self.assertFalse(form.is_valid())
+
+    #Tests for Staff Verification
+    def test_StaffUserForm_no_data(self):
+        form = NewUserForm(data={})
+        self.assertFalse(form.is_valid())
+    def test_StaffUserForm_good_data(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : 'Campus123', "password2":'Campus123', "staffVerification":"54321"})
+        self.assertTrue(form.is_valid())
+    def test_StaffUserForm_good_data_blank(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : 'Campus123', "password2":'Campus123', "staffVerification":""})
+        self.assertTrue(form.is_valid())
+    def test_StaffUserForm_bad_data(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : '12345', "password2":'12345'})
+        self.assertFalse(form.is_valid())
