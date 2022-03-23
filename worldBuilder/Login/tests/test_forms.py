6c83bc1273d9a61a0f1c5d070fefac3e0aeade29
Dean Kelly <66389284+KeanDelly@users.noreply.github.com>
2022-03-23 12:56:19 +0000

    Added functionality to register Gamemaster
    Game Master functionality added to allow for a user to register as game master based off a staff token

--- worldBuilder/Login/tests/test_forms.py
+++ worldBuilder/Login/tests/test_forms.py
@@ -4,6 +4,7 @@ from Login.forms import NewUserForm
 import datetime
 
 class TestForm(TestCase):
+    #Base Tests from Before Staff Verification
     def test_NewUserForm_no_data(self):
         form = NewUserForm(data={})
         self.assertFalse(form.is_valid())
@@ -13,3 +14,17 @@ class TestForm(TestCase):
     def test_NewUserForm_bad_data(self):
         form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : '12345', "password2":'12345'})
         self.assertFalse(form.is_valid())
+
+     #Tests for Staff Verification
+    def test_StaffUserForm_no_data(self):
+        form = NewUserForm(data={})
+        self.assertFalse(form.is_valid())
+    def test_StaffUserForm_good_data(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : 'Campus123', "password2":'Campus123', "staffVerification":"54321"})
+        self.assertTrue(form.is_valid())
+    def test_StaffUserForm_good_data_blank(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : 'Campus123', "password2":'Campus123', "staffVerification":""})
+        self.assertTrue(form.is_valid())
+    def test_StaffUserForm_bad_data(self):
+        form = NewUserForm(data = {"username":'Name1', "email":'email1@gmail.com',"password1" : '12345', "password2":'12345'})
+        self.assertFalse(form.is_valid()) 
