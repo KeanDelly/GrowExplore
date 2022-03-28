6c589fedeae7a8d285e9918ce9e4412a3008af8f
umennear <75434681+umennear@users.noreply.github.com>
2022-03-28 11:51:47 +0100

    Update test_URLS.py

--- worldBuilder/gardenGame/tests/test_URLS.py
+++ worldBuilder/gardenGame/tests/test_URLS.py
@@ -6,8 +6,6 @@ class URLTests(TestCase):
     def test_GardenGame(self):
         response = self.client.get('/main/') ## gets a response from the server and stores it
         self.assertEqual(response.status_code, 200) ## checks if it worked
-        response = self.client.get('/main/profile/') ## gets a response from the server and stores it
-        self.assertEqual(response.status_code, 200) ## checks if it worked
         response = self.client.get('/main/buildingOfTheDay/') ## gets a response from the server and stores it
         self.assertEqual(response.status_code, 200)  ## checks if it worked
         response = self.client.get('/main/reportToAdmin/')  ## gets a response from the server and stores it
